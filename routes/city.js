2955c38a3d0e22c4fe3880ce60209491684bf331
German Pinilla <gpinilladev@gmail.com>
2021-10-08 14:27:47 -0500

    BPSC-INIT-REPO #comment Commit inicial Steam Cell - backend NodeJS y Express

---
+++ routes/city.js
@@ -0,0 +1,11 @@
+let express = require("express");
+let City = require("../controllers/city");
+let api = express.Router();
+
+api.post("/city/add", City.addCity);
+api.get("/city/list", City.listCities);
+api.post("/city/:name?", City.listCities);
+api.put('/city/:id', City.updateCity);
+api.delete('/city/:id', City.deleteCity);
+
+module.exports = api;
